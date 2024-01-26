d26912e380c89560014665e6c673edcb6aacc65d
Dillon Min <dillon.minfei@gmail.com>
2024-01-26 22:44:51 +0800

    usr-wifi-232-t at command ok!
    Signed-off-by: Dillon Min <dillon.minfei@gmail.com>

--- examples/ili9325-on-gpio.rs
+++ examples/ili9325-on-gpio.rs
@@ -28,6 +28,149 @@ use embedded_hal::blocking::delay::{DelayMs, DelayUs};
 use embedded_hal::digital::v2::{OutputPin, InputPin};
 pub use display_interface::{DataFormat, DisplayError, WriteOnlyDataCommand};
 
+/* for usart1/dma2 */
+use stm32f4xx_hal::dma::{DmaFlag, PeripheralToMemory, Stream2};
+use stm32f4xx_hal::dma::config::DmaConfig;
+use stm32f4xx_hal::pac::{DMA2};
+use stm32f4xx_hal::pac::Interrupt;
+use stm32f4xx_hal::uart::config::StopBits;
+use stm32f4xx_hal::uart::{Config, Rx, Serial};
+use stm32f4xx_hal::{
+    dma::{StreamsTuple, Transfer},
+    pac,
+    prelude::*,
+    serial,
+};
+
+const UART_BUFFER_SIZE: usize = 4096;
+
+// Simple ring buffer
+pub struct Buffer {
+    buffer: [u8; UART_BUFFER_SIZE],
+    write_idx: usize,
+    read_idx: usize,
+}
+
+impl Buffer {
+    pub(crate) const fn new() -> Buffer {
+        Buffer {
+            buffer: [0; UART_BUFFER_SIZE],
+            write_idx: 0,
+            read_idx: 0,
+        }
+    }
+
+    pub fn push(&mut self, data: u8) {
+        self.buffer[self.write_idx] = data;
+        self.write_idx = (self.write_idx + 1) % UART_BUFFER_SIZE;
+    }
+
+    pub fn read(&mut self) -> Option<u8> {
+        if self.write_idx != self.read_idx {
+            let data = self.buffer[self.read_idx];
+            self.read_idx = (self.read_idx + 1) % UART_BUFFER_SIZE;
+            Some(data)
+        } else {
+            None
+        }
+    }
+}
+
+// dma type, needs to be adapted for uart and dma channel
+type UartDma = Transfer<
+    Stream2<pac::DMA2>,
+    4,
+    Rx<pac::USART1>,
+    PeripheralToMemory,
+    &'static mut [u8; UART_BUFFER_SIZE],
+>;
+
+// shared dma reference
+pub static G_TRANSFER: Mutex<RefCell<Option<UartDma>>> = Mutex::new(RefCell::new(None));
+
+// shared uart1 reference
+pub static G_UART1_BUFFER: Mutex<RefCell<Option<Buffer>>> = Mutex::new(RefCell::new(None));
+
+// shared TX reference
+pub static G_UART1_TX: Mutex<RefCell<Option<serial::Tx<pac::USART1>>>> =
+    Mutex::new(RefCell::new(None));
+
+// dma buffer
+pub static mut RX_UART1_BUFFER: [u8; UART_BUFFER_SIZE] = [0; UART_BUFFER_SIZE];
+
+pub fn uart1_read() -> Option<[u8; UART_BUFFER_SIZE]> {
+    let r = free(|cs| {
+        if let Some(buffer) = G_UART1_BUFFER.borrow(cs).borrow_mut().as_mut() {
+            let mut buf = [0; UART_BUFFER_SIZE];
+            let mut i = 0;
+            while let Some(byte) = buffer.read() {
+                if i < UART_BUFFER_SIZE - 1 {
+                    buf[i] = byte;
+                } else {
+                    break;
+                }
+                i += 1;
+            }
+            Some(buf)
+        } else {
+            None
+        }
+    });
+    r
+}
+// a wrapper function that reads out of the uart ring buffer
+pub fn uart1_read_until(eol: u8) -> Option<[u8; UART_BUFFER_SIZE]> {
+    let r = free(|cs| {
+        if let Some(buffer) = G_UART1_BUFFER.borrow(cs).borrow_mut().as_mut() {
+            let mut buf = [0; UART_BUFFER_SIZE];
+            let mut i = 0;
+            while let Some(byte) = buffer.read() {
+                if byte == eol {
+                    break;
+                }
+                if i < UART_BUFFER_SIZE - 1 {
+                    buf[i] = byte;
+                } else {
+                    break;
+                }
+                i += 1;
+            }
+            if buf[0] == 0 {
+                return None;
+            }
+            Some(buf)
+        } else {
+            None
+        }
+    });
+    r
+}
+
+// a wrapper function for uart write
+pub fn uart1_write(data: &[u8]) -> Result<(), serial::Error> {
+    free(|cs| {
+        let ret = if let Some(uart) = G_UART1_TX.borrow(cs).borrow_mut().as_mut() {
+            let non_zero_len = data
+                .iter()
+                .rposition(|&x| x != 0)
+                .map(|idx| idx + 1)
+                .unwrap_or(0);
+            // Create a custom slice with only non-zero elements
+            let mut led_ref = LED2.borrow(cs).borrow_mut();
+            if let Some(ref mut led) = led_ref.deref_mut() {
+                led.toggle();
+            }
+            uart.bwrite_all(&data[0..non_zero_len])?;
+            uart.bflush()
+        } else {
+            Err(serial::Error::Other)
+        };
+        ret
+    })
+}
+
+/* for usart1/dma2 */
+
 type ResultPin<T = ()> = core::result::Result<T, DisplayError>;
 
 pub struct TouchStm32GpioIntf<CS, CLK, DIN, DOUT> {
@@ -252,6 +395,64 @@ fn main() -> ! {
         .freeze();
     let mut syscfg = dp.SYSCFG.constrain();
 
+    /* for usart1/dma2 */
+    let gpioa = dp.GPIOA.split();
+    let dma2 = StreamsTuple::new(dp.DMA2);
+    // configure UART, it is important to configure this to use DMA
+    let rx_1 = gpioa.pa10.into_alternate();
+    let tx_1 = gpioa.pa9.into_alternate();
+    let uart1 = Serial::new(
+         dp.USART1,
+         (tx_1, rx_1),
+         Config::default()
+             .baudrate(115200.bps())
+             .parity_none()
+             .stopbits(StopBits::STOP1)
+             .dma(serial::config::DmaConfig::Rx),
+         &clocks,
+    )
+    .unwrap();
+
+    // Note! It is better to use memory pools, such as heapless::pool::Pool. But it not work with embedded_dma yet.
+    // See CHANGELOG of unreleased main branch and issue https://github.com/japaric/heapless/pull/362 for details.
+    let rx_buffer1 =
+            cortex_m::singleton!(: [u8; UART_BUFFER_SIZE] = [0; UART_BUFFER_SIZE]).unwrap();
+    let _rx_buffer2 =
+            cortex_m::singleton!(: [u8; UART_BUFFER_SIZE] = [0; UART_BUFFER_SIZE]).unwrap();
+
+    let (tx1, mut rx) = uart1.split();
+
+    rx.listen_idle();
+
+    free(|cs| *G_UART1_TX.borrow(cs).borrow_mut() = Some(tx1));
+
+    free(|cs| {
+            *G_UART1_BUFFER.borrow(cs).borrow_mut() = Some(Buffer::new());
+    });
+    // Initialize and start DMA stream
+    let mut rx_transfer = Transfer::init_peripheral_to_memory(
+            dma2.2,
+            rx,
+            rx_buffer1,
+            None,
+            DmaConfig::default()
+                .memory_increment(true)
+                .fifo_enable(true)
+                .fifo_error_interrupt(true)
+                .transfer_complete_interrupt(true),
+        );
+
+    rx_transfer.start(|_rx| {});
+
+    cortex_m::interrupt::free(|cs| *G_TRANSFER.borrow(cs).borrow_mut() = Some(rx_transfer));
+
+    // Enable interrupt
+    unsafe {
+            cortex_m::peripheral::NVIC::unmask(Interrupt::USART1);
+            cortex_m::peripheral::NVIC::unmask(pac::Interrupt::DMA2_STREAM2);
+    }
+    /* for usart1/dma2 */
+    
     // Create a button input with an interrupt
     let gpioc = dp.GPIOC.split();
     let mut touch_int = gpioc.pc1.into_pull_up_input();
@@ -271,7 +472,7 @@ fn main() -> ! {
     };
     let mut delay = cp.SYST.delay(&clocks);
     //let gpioc = dp.GPIOC.split();
-    let gpioa = dp.GPIOA.split();
+    //let gpioa = dp.GPIOA.split();
     let mut led1 = gpioa.pa11.into_push_pull_output();
     let led2 = gpioa.pa12.into_push_pull_output();
     free(|cs| {
@@ -279,6 +480,19 @@ fn main() -> ! {
     });
     let mut tx = dp.USART2.tx(gpioa.pa2, 115200.bps(), &clocks).unwrap();
     writeln!(tx, "ILI9325 Lcd\r").unwrap();
+    uart1_write(b"+++").unwrap();
+    //delay.delay_ms(100_u32);
+    let response = uart1_read().expect("Failed to read from UART1");
+    writeln!(tx, "usr read: {}\r", core::str::from_utf8(&response).unwrap()).unwrap();
+    uart1_write(b"a").unwrap();
+    //delay.delay_ms(100_u32);
+    let response = uart1_read().expect("Failed to read from UART1");
+    writeln!(tx, "usr read: {}\r", core::str::from_utf8(&response).unwrap()).unwrap();
+    //delay.delay_ms(100_u32);*/
+    uart1_write(b"AT+H\r").unwrap();
+    delay.delay_ms(400_u32);
+    let response = uart1_read().expect("Failed to read from UART1");
+    writeln!(tx, "usr read: {}\r", core::str::from_utf8(&response).unwrap()).unwrap();
     let interface = ParallelStm32GpioIntf::new(
                                               &mut delay,
                                               &mut tx,
@@ -428,5 +642,50 @@ fn EXTI1() {
 //            }
 
             }
+    });
+}
+
+#[interrupt]
+#[allow(non_snake_case)]
+fn USART1() {
+    free(|cs| {
+        if let Some(transfer) = G_TRANSFER.borrow(cs).borrow_mut().as_mut() {
+            if transfer.is_idle() {
+            let mut led_ref = LED2.borrow(cs).borrow_mut();
+                // Calc received bytes count
+                let bytes_count = UART_BUFFER_SIZE - transfer.number_of_transfers() as usize;
+                unsafe {
+                    let mut buffer = [0; UART_BUFFER_SIZE];
+                    match transfer.next_transfer(&mut RX_UART1_BUFFER) {
+                        Ok((b, _)) => buffer = *b,
+                        Err(_err) => {}
+                    }
+                    if let Some(ring_buffer) = G_UART1_BUFFER.borrow(cs).borrow_mut().as_mut() {
+                        for i in 0..bytes_count {
+                            if let Some(ref mut led) = led_ref.deref_mut() {
+                                led.toggle();
+                            }
+                            ring_buffer.push(buffer[i]);
+                        }
+                    }
+                }
+            }
+            transfer.clear_idle_interrupt();
+        }
+    });
+}
+
+#[interrupt]
+#[allow(non_snake_case)]
+fn DMA2_STREAM2() {
+    free(|cs| {
+            let mut led_ref = LED2.borrow(cs).borrow_mut();
+            if let Some(ref mut led) = led_ref.deref_mut() {
+                led.toggle();
+            }
+        if let Some(transfer) = G_TRANSFER.borrow(cs).borrow_mut().as_mut() {
+            // Its important to clear fifo errors as the transfer is paused until it is cleared
+            transfer.clear_flags(DmaFlag::FifoError | DmaFlag::TransferComplete);
+        }
     });
 }
